e400353eea7178ea85ac2ec3be7772f01896849b
abhishekY2401 <abhiyd2401@gmail.com>
2022-02-12 19:40:56 +0530

    Added routing paths

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App";
+import Home from "./components/HomePage/Home";
+import About from "./components/About Us/About";
+import Login from "./components/LoginPage/Login";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/about-us" element={<About />} />
+    </Routes>
   </BrowserRouter>,
   rootElement
 );

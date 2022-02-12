61c5a4a865cd00fd66e0b14a1e4e1c2319eeb52e
abhishekY2401 <abhiyd2401@gmail.com>
2022-02-12 14:59:06 +0530

    Made changes

--- src/components/homePage/Home.js
+++ src/components/homePage/Home.js
@@ -1,5 +1,11 @@
+import Header from "./components/HeaderPage/Header";
+
 const Home = () => {
-  return <div class="main"></div>;
+  return (
+    <div class="main">
+      <Header />
+    </div>
+  );
 };
 
 export default Home;

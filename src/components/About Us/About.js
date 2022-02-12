609d0d692e45d31e516ae9a7aab83d17a37370b0
Jainam <jbchheda03@gmail.com>
2022-02-12 20:40:37 +0530

    Changes about.css

--- src/components/About Us/About.js
+++ src/components/About Us/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div>
       <header>
-        <h1> About Us </h1>
+        <h1 id="about"> About Us </h1>
       </header>
       <br />
       <div id="container1">
@@ -40,7 +40,7 @@ const About = () => {
         <div id="container2">
           <header id="newhead">
             <br />
-            <h1> CONTACT US </h1>
+            <h1 id="contact"> CONTACT US </h1>
           </header>
           <br /> <br />
           <span>

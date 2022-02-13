3cdff0161aabba22dce0d7fdca28789cffb7e8d1
abhishekY2401 <abhiyd2401@gmail.com>
2022-02-13 10:55:15 +0530

    Added 2 questions

--- src/components/FeedbackPage/Form.js
+++ src/components/FeedbackPage/Form.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 import "./Form.css";
 import Question from "./Question";
 import Report from "./Report";
 
 const Form = () => {
-  // a form data handler to handle data
-  const generateReport = () => {};
+  const [formData, setFormData] = useState([]);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+
+      // Trimming any whitespace
+      [e.target.name]: e.target.value.trim(),
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
 
   return (
     <div className="form-box">
@@ -20,33 +35,46 @@ const Form = () => {
         </p>
       </div>
 
-      <Question
-        question="What soft-skills does the candidate possess ?"
-        value1="Communication"
-        value2="Story Teller"
-        value3="Clear Thoughts"
-        value4="Attitude"
-      />
+      <div className="questions">
+        <Question
+          question="What soft-skills does the candidate possess ?"
+          value1="Communication"
+          value2="Story Teller"
+          value3="Clear Thoughts"
+          value4="Attitude"
+        />
 
-      <Question
-        question="Which tech-skills does the candidate has?"
-        value1="Java"
-        value2="AWS"
-        value3="NextJS"
-        value4="React"
-      />
+        <Question
+          question="Which tech-skills does the candidate has?"
+          value1="Java"
+          value2="AWS"
+          value3="NextJS"
+          value4="React"
+        />
 
-      <Question
-        question="What are some points you think that the candidate lacks ?"
-        value1="Responsive"
-        value2="Friendly Relation"
-        value3="Problem Solving"
-        value4="Confidence"
-      />
+        <Question
+          question="What are some points you think that the candidate lacks ?"
+          value1="Responsive"
+          value2="Friendly Relation"
+          value3="Problem Solving"
+          value4="Confidence"
+        />
 
-      <button type="submit" class="btn btn-primary" onClick={generateReport}>
-        Generate
-      </button>
+        <Question
+          question="How does the candidate acts in the challenging situation?"
+          value1="fearing"
+          value2="silent thinker"
+          value3="confident answering"
+          value4="give up"
+        />
+
+        <label>Do you have anything to say to the candidate?</label>
+        <input type="text" placeholder="Any thoughts.." id="thoughts" />
+
+        <button type="submit" class="btn btn-primary" onClick={handleSubmit}>
+          Generate
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/FeedbackPage/Question.js
+++ src/components/FeedbackPage/Question.js
@@ -4,6 +4,18 @@ const Question = (props) => {
   // making a boolean check value
   // const [checked, setChecked] = useState(false);
 
+  // a state to hold all the checked answers
+  const [value, setValue] = useState([
+    { question1: "What soft-skills does the candidate possess ?", answer: [] },
+    { question2: "Which tech-skills does the candidate has? ", answer: [] },
+    {
+      question3: "What are some points you think that the candidate lacks ?",
+      answer: [],
+    },
+    { question4: "", answer: [] },
+    { question5: "", answer: [] },
+  ]);
+
   // a list to hold the id of each checkboxes
   const [slider, setSlider] = useState([
     { id: 0, checked: false },
@@ -16,14 +28,24 @@ const Question = (props) => {
     for (let i = 0; i < slider.length; i++) {
       // if the checked is false show the slider
       if (id == slider[i].id && slider[i].checked == true) {
-        setSlider((prevState) => [...prevState, (slider[i].checked = false)]);
+        setSlider(() => [...slider, (slider[i].checked = false)]);
+        console.log(slider);
       }
 
       // if the checked is true hide the slider
       if (id == slider[i].id && slider[i].checked == false) {
-        setSlider((prevState) => [...prevState, (slider[i].checked = true)]);
+        setSlider(() => [...slider, (slider[i].checked = true)]);
+        console.log(slider);
       }
     }
+  };
+
+  const getValue = (e) => {
+    const val = e.target.value;
+
+    const parentText = e.target.parentNode;
+    console.log(parentText.value);
+    setValue(val);
   };
 
   return (
@@ -35,7 +57,11 @@ const Question = (props) => {
           type="checkbox"
           value={props.value1}
           id="0"
-          onChange={() => enableSlider(0)}
+          onChange={(e) => {
+            enableSlider(0);
+            getValue(e);
+          }}
+          required
         />
         <label>{props.value1}</label>
         {slider[0].checked && <input type="range" min="0" max="10" />}
@@ -46,7 +72,11 @@ const Question = (props) => {
           type="checkbox"
           value={props.value2}
           id="1"
-          onChange={() => enableSlider(1)}
+          onChange={(e) => {
+            enableSlider(1);
+            getValue(e);
+          }}
+          required
         />
         <label>{props.value2}</label>
         {slider[1].checked && <input type="range" min="0" max="10" />}
@@ -57,7 +87,11 @@ const Question = (props) => {
           type="checkbox"
           value={props.value3}
           id="2"
-          onChange={() => enableSlider(2)}
+          onChange={(e) => {
+            enableSlider(2);
+            getValue(e);
+          }}
+          required
         />
         <label>{props.value3}</label>
         {slider[2].checked && <input type="range" min="0" max="10" />}
@@ -68,7 +102,11 @@ const Question = (props) => {
           type="checkbox"
           value={props.value4}
           id="3"
-          onChange={() => enableSlider(3)}
+          onChange={(e) => {
+            enableSlider(3);
+            getValue(e);
+          }}
+          required
         />
         <label>{props.value4}</label>
         {slider[3].checked && <input type="range" min="0" max="10" />}
